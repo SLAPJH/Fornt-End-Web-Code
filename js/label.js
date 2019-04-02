cec292a064169f02ee0e0d68b8c917c5003717fe
Park Hyun Sang <36691734+HyunSang0625@users.noreply.github.com>
2019-04-02 17:47:42 +0900

    JavaScript

---
+++ js/label.js
@@ -0,0 +1,16 @@
+// Input Email, Password 포커스
+$(document).ready(function() {
+    var placeholderTarget = $('.textbox input[type="email"], .textbox input[type="password"]');
+    
+    //포커스시
+    placeholderTarget.on('focus', function(){
+      $(this).siblings('label').fadeOut('fast');
+    });
+  
+    //포커스아웃시
+    placeholderTarget.on('focusout', function(){
+      if($(this).val() == ''){
+        $(this).siblings('label').fadeIn('fast');
+      }
+    });
+  });
---
+++ js/main-js.js
@@ -0,0 +1,8 @@
+// Main JavaScript
+
+// 아이디 찾기 
+function popupOpenID() {
+    var popUrl = "/html/popup.html";
+    var popOption = "widht=370px, height= 400px"; //팝업창 옵션
+        window.open(popupOpen(),"",popOption);
+}
